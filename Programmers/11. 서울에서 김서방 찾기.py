a33243408fd32bbcad1c774be8065d640c93b7d8
JinOh <pinion0801@gamil.com>
2021-01-08 17:21:32 +0900

    2021.01.08

---
+++ Programmers/11. 서울에서 김서방 찾기.py
@@ -0,0 +1,9 @@
+"""
+Min : 0.00ms, 10.2MB
+Max : 0.01ms, 10.3MB
+"""
+
+def solution(seoul):
+    location = seoul.index("Kim")
+    answer = f"김서방은 {location}에 있다"
+    return answer
---
+++ Programmers/12. 수박수박수박수박수박수박수.py
@@ -0,0 +1,7 @@
+"""
+Min : 0.00ms, 10.1MB
+Max : 0.01ms, 10.2MB
+"""
+
+def solution(n):
+    return "수박"*(n//2) + "수"*(n%2)
